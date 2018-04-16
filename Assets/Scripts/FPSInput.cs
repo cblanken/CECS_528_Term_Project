0da87220aff3fc3be1d9833cdba3c897ca2f42d9
[Cameron Blankenbuehler] <[cameron.blankenbuehler@gmail.com]>
2018-04-15 21:17:25 -0400

    Second scene with basic layout for level_02 added.

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -9,8 +9,9 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 [AddComponentMenu("Control Script/FPS Input")]
 public class FPSInput : MonoBehaviour {
-	public float speed = 9.0f;
+	public float _speed = 9.0f;
 	public float gravity = -9.8f;
+    [SerializeField] private float JumpHeight = 4;
     [SerializeField] private Vector3 _velocity;
 	private CharacterController _charController;
 
@@ -25,32 +26,31 @@ public class FPSInput : MonoBehaviour {
             _velocity.y = 0f;
         }
 
-        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-		float deltaZ = Input.GetAxis("Vertical") * speed;
+        //transform.Translate(Input.GetAxis("Horizontal") * _speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * _speed * Time.deltaTime);
+        float deltaX = Input.GetAxis("Horizontal") * _speed;
+		float deltaZ = Input.GetAxis("Vertical") * _speed;
 		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-		movement = Vector3.ClampMagnitude(movement, speed);
+		movement = Vector3.ClampMagnitude(movement, _speed);
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
         _charController.Move(movement);
 
-
+        // Apply gravity
         _velocity.y += gravity * Time.deltaTime;
         _velocity = Vector3.ClampMagnitude(_velocity, 53);
         _charController.Move(_velocity * 1.5f * Time.deltaTime);
 
 
-
-
-
-
-
-
         // Sprint function
         if (Input.GetKey (KeyCode.LeftShift) && _charController.isGrounded) {
-			speed = 14.0f;
+			_speed = 14.0f;
 		} else {
-			speed = 7.0f;
+			_speed = 7.0f;
 		}
+
+        // Jump function
+        if (Input.GetButtonDown("Jump") && _charController.isGrounded){
+            _velocity.y += Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
 	}
 }
