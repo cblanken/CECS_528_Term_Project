c3404fe83aca630cc8eef3397470b37755d3aa6a
[Cameron Blankenbuehler] <[cameron.blankenbuehler@gmail.com]>
2018-04-23 13:24:34 -0400

    Fix FPS Input script.

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -8,35 +8,56 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 [AddComponentMenu("Control Script/FPS Input")]
-public class FPSInput : MonoBehaviour {
-	public float speed = 9.0f;
-	public float gravity = -9.8f;
-	private CharacterController _charController;
+public class FPSInput : MonoBehaviour
+{
+    public float _speed = 9.0f;
+    public float gravity = -9.8f;
+    [SerializeField] private float JumpHeight = 4;
+    [SerializeField] private Vector3 _velocity;
+    private CharacterController _charController;
 
 
-	void Start() {
-		_charController = GetComponent<CharacterController>();
-	}
-	
-	void Update() {
-		//transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-		float deltaX = Input.GetAxis("Horizontal") * speed;
-		float deltaZ = Input.GetAxis("Vertical") * speed;
-		Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-		movement = Vector3.ClampMagnitude(movement, speed);
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
 
-		movement.y = gravity;
+    void Update()
+    {
+        if (_charController.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = 0f;
+        }
 
-		movement *= Time.deltaTime;
-		movement = transform.TransformDirection(movement);
-		_charController.Move(movement);
+        //transform.Translate(Input.GetAxis("Horizontal") * _speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * _speed * Time.deltaTime);
+        float deltaX = Input.GetAxis("Horizontal") * _speed;
+        float deltaZ = Input.GetAxis("Vertical") * _speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, _speed);
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+
+        // Apply gravity
+        _velocity.y += gravity * Time.deltaTime;
+        _velocity = Vector3.ClampMagnitude(_velocity, 53);
+        _charController.Move(_velocity * 1.5f * Time.deltaTime);
 
 
-		// Sprint function
-		if (Input.GetKey (KeyCode.LeftShift) && _charController.isGrounded) {
-			speed = 14.0f;
-		} else {
-			speed = 7.0f;
-		}
-	}
+        // Sprint function
+        if (Input.GetKey(KeyCode.LeftShift) && _charController.isGrounded)
+        {
+            _speed = 14.0f;
+        }
+        else
+        {
+            _speed = 7.0f;
+        }
+
+        // Jump function
+        if (Input.GetButtonDown("Jump") && _charController.isGrounded)
+        {
+            _velocity.y += Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+    }
 }
