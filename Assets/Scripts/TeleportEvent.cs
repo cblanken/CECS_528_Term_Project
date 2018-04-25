ba10458e3851e95561280e80c6075382b51ea109
bsvanb01 <34143024+bsvanb01@users.noreply.github.com>
2018-04-24 22:17:44 -0400

    Teleport Events
    Teleport Events

---
+++ Assets/Scripts/TeleportEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            Application.LoadLevel("Level_02");
+    }
+}
---
+++ Assets/Scripts/TeleportEvent2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportEvent2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            Application.LoadLevel("Level_03");
+    }
+}
