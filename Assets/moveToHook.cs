a6011ef718b28d6d3766a4155265863bf134b12a
Zac Revers <zrevers521@gmail.com>
2018-04-24 20:31:53 -0400

    added grappling lightning effect

---
+++ Assets/moveToHook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToHook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject hookHolder = GameObject.Find("Hook");
+		this.transform.position = new Vector3(hookHolder.transform.position.x, hookHolder.transform.position.y, hookHolder.transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject hookHolder = GameObject.Find("Hook");
+        this.transform.position = new Vector3(hookHolder.transform.position.x, hookHolder.transform.position.y, hookHolder.transform.position.z);
+    }
+}
