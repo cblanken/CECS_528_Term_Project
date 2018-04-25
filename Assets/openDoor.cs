f4bac5f839c68dfc1b8307f72d54bc3e51ad163e
Zac Revers <zrevers521@gmail.com>
2018-04-25 10:59:43 -0400

    added switch for invisible door

---
+++ Assets/openDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject door = GameObject.Find("Door");
+            door.SetActive(false);
+        }
+    }
+}
